9191b705b4dc15785f3dace239890bb06a79705e
Igor <samoilovip@mail.ru>
2018-08-31 00:02:00 +0300

    -Рефторинг

--- Tanks/menu-window.cpp
+++ Tanks/menu-window.cpp
@@ -1,7 +1,10 @@
 #include "menu-window.h"
 #include <functional>
 
-#define Prepare_Callback(in_Pointer, in_Method) [in_Pointer] (Button *in_Object) -> void { (in_Pointer->*in_Method)(in_Object); }
+#define PREPARE_CALLBACK(in_Pointer, in_Method) [in_Pointer] (Button *in_Object) -> void { (in_Pointer->*in_Method)(in_Object); }
+
+#define BUTTON_SIZE sf::Vector2f(120, 40)
+#define BUTTON_OFFSET_Y 10
 
 MenuWindow::MenuWindow(Engine *in_Engine, const sf::Vector2f &in_Pos, const sf::Vector2f &in_Size) : Window<Widget>(in_Pos, in_Size)
 {
@@ -9,19 +12,19 @@ MenuWindow::MenuWindow(Engine *in_Engine, const sf::Vector2f &in_Pos, const sf::
 
 	Button *button;
 
-	button = new Button(sf::Vector2f(GetSize().x / 2, GetSize().y / 2 - 60), sf::Vector2f(120, 40));
+	button = new Button(sf::Vector2f(GetSize().x / 2 - BUTTON_SIZE.x / 2, GetSize().y / 2 - (BUTTON_SIZE.y + BUTTON_OFFSET_Y)), BUTTON_SIZE);
 	button->SetText(L"Играть");
-	button->SetOnClickedListener(Prepare_Callback(this, &MenuWindow::onStartClicked));//  Prepare_Callback(this, MenuWindow::onStartClicked));
+	button->SetOnClickedListener(PREPARE_CALLBACK(this, &MenuWindow::onStartClicked));//  Prepare_Callback(this, MenuWindow::onStartClicked));
 	AddWidget(button);
 
-	button = new Button(sf::Vector2f(GetSize().x / 2, GetSize().y / 2), sf::Vector2f(120, 40));
+	button = new Button(sf::Vector2f(GetSize().x / 2 - BUTTON_SIZE.x / 2, GetSize().y / 2), BUTTON_SIZE);
 	button->SetText(L"Редактор");
-	button->SetOnClickedListener(Prepare_Callback(this, &MenuWindow::onEditorClicked));
+	button->SetOnClickedListener(PREPARE_CALLBACK(this, &MenuWindow::onEditorClicked));
 	AddWidget(button);
 
-	button = new Button(sf::Vector2f(GetSize().x / 2, GetSize().y / 2 + 60), sf::Vector2f(120, 40));
+	button = new Button(sf::Vector2f(GetSize().x / 2 - BUTTON_SIZE.x / 2, GetSize().y / 2 + (BUTTON_SIZE.y + BUTTON_OFFSET_Y)), BUTTON_SIZE);
 	button->SetText(L"Выход");
-	button->SetOnClickedListener(Prepare_Callback(this, &MenuWindow::onExitClicked));
+	button->SetOnClickedListener(PREPARE_CALLBACK(this, &MenuWindow::onExitClicked));
 	AddWidget(button);
 }
 
