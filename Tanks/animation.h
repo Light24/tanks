8e65dad44877ce1f7c9ddf06203a650ccc49807b
Igor <samoilovip@mail.ru>
2018-09-02 20:09:17 +0300

    fix

--- Tanks/animation.h
+++ Tanks/animation.h
@@ -1,8 +1,27 @@
 #pragma once
 
+enum Animation_Type
+{
+	Idle,
+	Top,
+	Bottom,
+	Left,
+	Right,
+};
+
 class Animation
 {
 public:
 	Animation();
 	~Animation();
+
+public:
+	void LoadAnimation();
+
+	void SetAnimationType(Animation_Type in_AnimationType);
+
+	void Draw(sf::RenderWindow *in_RenderWindow);
+
+private:
+	Animation_Type m_AnimationType;
 };
